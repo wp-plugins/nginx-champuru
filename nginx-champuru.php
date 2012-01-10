7352d6a677026629f6f4f39c16088c0d143ef3cf
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-10 08:04:52 +0000

    ver 0.4.0
    git-svn-id: https://plugins.svn.wordpress.org/nginx-champuru/trunk@487391 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nginx-champuru.php
+++ nginx-champuru.php
@@ -4,7 +4,7 @@ Plugin Name: Nginx Champuru
 Author: Takayuki Miyauchi
 Plugin URI: http://firegoby.theta.ne.jp/wp/nginx-champuru
 Description: Plugin for Nginx Reverse Proxy 
-Version: 0.2.0
+Version: 0.4.0
 Author URI: http://firegoby.theta.ne.jp/
 Domain Path: /languages
 Text Domain: nginx-champuru
@@ -19,6 +19,7 @@ class NginxChampuru {
 
 private $js_version = "0.2.0";
 private $query = "nginxchampuru";
+private $cache_dir = '/var/cache/nginx';
 
 function __construct()
 {
@@ -37,28 +38,59 @@ function __construct()
         array(&$this, "wp_get_current_commenter"),
         9999
     );
+    add_action("publish_future_post ", array(&$this, "flush_caches"));
+    add_action("save_post", array(&$this, "flush_caches"));
+    add_action("comment_post", array(&$this, "flush_caches"));
+    add_action("wp_set_comment_status", array(&$this, "flush_caches"));
+    add_filter("got_rewrite", "__return_true");
+    add_filter("pre_comment_user_ip", "pre_comment_user_ip");
+}
+
+public function pre_comment_user_ip()
+{
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $X_FORWARDED_FOR = explode(",", $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $REMOTE_ADDR = trim($X_FORWARDED_FOR[0]);
+    } else {
+        $REMOTE_ADDR = $_SERVER['REMOTE_ADDR'];
+    }
+    return $REMOTE_ADDR;
+}
+
+public function flush_caches()
+{
+    if (defined("NGINX_DELETE_CACHES") && NGINX_DELETE_CACHES) {
+        if (defined("NGINX_CACHE_DIR") && NGINX_CACHE_DIR) {
+            $this->cache_dir = NGINX_CACHE_DIR;
+        }
+        $cmd = sprintf(
+            'grep -lr %s %s | xargs rm -f &> /dev/null &',
+            escapeshellarg(home_url()),
+            escapeshellarg($this->cache_dir)
+        );
+        exec($cmd);
+    }
 }
 
 public function wp()
 {
     global $post;
-    if (post_password_required($post->ID)) {
+    if (is_singular() && post_password_required($post->ID)) {
         nocache_headers();
     }
 }
 
 public function wp_enqueue_scripts()
 {
-    if (!comments_open()) {
-        return;
+    if ((is_singular() && comments_open()) || $this->is_future_post()) {
+        wp_enqueue_script(
+            'nginx-champuru',
+            plugins_url('/nginx-champuru.js', __FILE__),
+            array('jquery'),
+            $this->js_version,
+            true
+        );
     }
-    wp_enqueue_script(
-        'nginx-champuru',
-        plugins_url('/nginx-champuru.js', __FILE__),
-        array('jquery'),
-        $this->js_version,
-        true
-    );
 }
 
 public function get_commenter_json()
@@ -80,6 +112,20 @@ public function wp_get_current_commenter($commenter)
             'comment_author_url'   => '',
         );
     }
+}
+
+private function is_future_post()
+{
+    $cron = get_option("cron");
+    foreach ($cron as $key => $jobs) {
+        if (is_array($jobs)) {
+            $res = array_key_exists("publish_future_post", $jobs);
+            if ($res) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 }
